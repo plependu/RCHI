bdf865d472c1c7f6b7e3072c0efb2b5a352f6da0
Francisco Gallego <fgall002@ucr.edu>
2019-08-06 18:27:12 -0700

    Added the ability to import csv files, and exports the counts into csv by creating a Data frame for the counts

--- HUDPointTemplate/VeteransHouseholdsWithAdultandChildren.py
+++ HUDPointTemplate/VeteransHouseholdsWithAdultandChildren.py
@@ -17,7 +17,108 @@ Wrote them based on the csv column 'Chronically Homeless Status'
 '''
 
 import pandas as pd
-in_df = pd.read_csv('../../../HouseholdQuestions_Cities_Districts_040119_1300.csv')
+# in_df = pd.read_csv('../../../HouseholdQuestions_Cities_Districts_040119_1300.csv')
+#*  Save to CSV FILE
+import tkinter as tk
+from tkinter import filedialog
+import os
+def exportCSV():
+    
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
+    if not export_file_path: # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    data_Table().to_csv (export_file_path, index = None, header=True)
+
+def importCSV():
+    global in_df
+    import_file_path = filedialog.askopenfilename(initialdir = os.getcwd(),filetypes=[("CSV Files",".csv")])
+    if import_file_path:
+        in_df = pd.read_csv(import_file_path)
+        print("Created")
+
+#* Helper Function to create tables for csv file
+def helperFunction_HouseHolds_Info():
+    total_num_of_households = total_number_of_households()
+    total_num_of_persons = total_number_of_persons()
+    total_num_of_Veterans = total_number_of_veterans()
+
+    data = {'Veteran Households with at Least One Adult and One Child':['Total number of households','Total number of persons','Total number of veterans']\
+            , 'Unsheltered': [total_num_of_households,total_num_of_persons,total_num_of_Veterans]}
+
+    df = pd.DataFrame(data)
+    df['Unsheltered'] = df['Unsheltered'].astype(int)
+    
+    return df
+
+def helperFunction_Gender():
+
+    female = total_number_of_female()
+    male =  total_number_of_male() 
+    transgender = total_number_of_transgender()
+    genderNonConforming = total_number_of_gender_non_conforming()
+
+    data = {'Gender (veterans only)':['Female (veterans only)', 'Male (veterans only)', 'Transgender (veterans only)', 'Gender Non-Conforming (veterans only)']\
+            , 'Unsheltered': [female,male,transgender,genderNonConforming]}
+
+    df = pd.DataFrame(data)
+    df['Unsheltered'] = df['Unsheltered'].astype(int)
+ 
+
+    return df
+
+def helperFunction_Ethnicity():
+
+    non_Latino = total_number_of_ethnicity_nonlatino()
+    Latino = total_number_of_ethnicity_latino() 
+
+    data = {'Ethnicity (veterans only)':['Non-Hispanic/Non-Latino (veterans only)', 'Hispanic/Latino (veterans only)']\
+        , 'Unsheltered': [non_Latino, Latino]}
+    
+    df = pd.DataFrame(data) 
+    df['Unsheltered'] = df['Unsheltered'].astype(int)
+
+    return df
+
+def helperFunction_Race():
+
+    white = total_number_of_race_white()
+    black = total_number_of_race_African() 
+    asian = total_number_of_race_Asian()
+    americanIndian = total_number_of_race_AmericanIndian()
+    nativeHawaiian = total_number_of_race_NativeHawiian()
+    multiple = total_number_of_race_Multiple()
+
+    data = {'Race (veterans only)':['White (veterans only)', 'Black or African-American (veterans only)', 'Asian (veterans only)', 'American Indian or Alaska Native (veterans only)','Native Hawaiian or Other Pacific Islander (veterans only)','Multiple Races (veterans only)']\
+        , 'Unsheltered': [white, black, asian, americanIndian, nativeHawaiian, multiple]}
+    
+    df = pd.DataFrame(data) 
+    df['Unsheltered'] = df['Unsheltered'].astype(int)
+
+    return df
+
+def helperFunction_ChronicallyHomeless():
+    total_num_of_chronicallyHomeless_households = total_number_of_ChronicallyHomeless_Households()
+    total_num_of_chronicallyHomeless = total_number_person_chronically_homeless()
+
+    data = {'Chronically Homeless':['Total number of households (Chronically Homeless)' , 'Total number of persons (Chronically Homeless)']\
+        , 'Unsheltered': [total_num_of_chronicallyHomeless_households, total_num_of_chronicallyHomeless]}
+
+    df = pd.DataFrame(data) 
+    df['Unsheltered'] = df['Unsheltered'].astype(int)
+
+    return df
+
+##* HelperFunction Combine Tables for CSV File
+def data_Table():
+    df0 = helperFunction_HouseHolds_Info()
+    df1 = helperFunction_Gender().rename(columns={"Gender (veterans only)": "Veteran Households with at Least One Adult and One Child"})
+    df2 = helperFunction_Ethnicity().rename(columns={"Ethnicity (veterans only)": "Veteran Households with at Least One Adult and One Child"})
+    df3 = helperFunction_Race().rename(columns={"Race (veterans only)": "Veteran Households with at Least One Adult and One Child"})
+    df4 = helperFunction_ChronicallyHomeless().rename(columns={"Chronically Homeless": "Veteran Households with at Least One Adult and One Child"})
+
+    result = df0.append([df1,df2, df3,df4]).reset_index(drop=True)
+    
+    return result
 
 ##* Helper Function that returns total number adults with child 
 def helperFunction_Total_num_Veterans_households():
@@ -267,7 +368,7 @@ def total_number_of_race_known():
 
 ## TODO Create some test cases 
 #* Total number of households (Chronically Homeless)
-def total_number_of_ChronicallyHomeless():
+def total_number_of_ChronicallyHomeless_Households():
 
     households_list =  helperFunction_Total_num_Veterans_households()
 
@@ -296,107 +397,15 @@ def total_number_person_chronically_homeless():
     
     return total_persons
 
-
-print("---------Unit Testing ---------")
-
-print('\n')
-print('--------Total Number Of HouseHolds------------')
-print("Total number of households: ", total_number_of_households())
-print('\n')
-
-print('--------Total Number Of Persons------------')
-print('Total number of persons: ', total_number_of_persons())
-print('\n')
-
-print('--------Total Number Of Veterans------------')
-print('Total number of veterans: ', total_number_of_veterans())
-print('\n')
-
-print("---------Gender (Veteran Only)---------")
-print('\n')
-print('--------Total Number Of Veterans Female------------')
-print('Total number of female veterans: ', total_number_of_female())
-print('\n')
-
-print('\n')
-print('--------Total Number Of Veterans Male------------')
-print('Total number of male veterans: ', total_number_of_male())
-print('\n')
-
-print('\n')
-print('--------Total Number Of Veterans Transgender------------')
-print('Total number of Transgender veterans: ', total_number_of_transgender())
-print('\n')
-
-print('\n')
-print('--------Total Number Of Veterans Gender Non-conforming------------')
-print('Total number of gender non-conforming veterans: ', total_number_of_gender_non_conforming())
-print('\n')
-
-print('\n')
-print('--------Total Number Of Veterans Gender Known------------')
-print('Total number of known veterans: ', total_number_of_gender_known())
-print('\n')
-
-print("---------Ethnicity (Veteran Only)---------")
-print('\n')
-
-print('--------Total Number Of Veterans Non-Hispanic/Non-Latino------------')
-print('Total number of non-hispanic/non-latino: ', total_number_of_ethnicity_nonlatino())
-print('\n')
-
-print('--------Total Number Of Veterans Hispanic/Latino------------')
-print('Total number of latino/hispanic ', total_number_of_ethnicity_latino())
-print('\n')
-
-print('--------Total Number Of Veterans Ethnicity Known------------')
-print('Total number of Ethnicity Known ', total_number_of_ethnicity_Known())
-print('\n')    
-
-
-print("---------Race (Veteran Only)---------")
-print('\n')
-
-print('--------Total Number Of White-----------')
-print('Total number White ', total_number_of_race_white())
-print('\n')
-
-print('--------Total Number Of Black or African American-----------')
-print('Total number Black or African American ', total_number_of_race_African())
-print('\n')
-
-print('--------Total Number Of Asian-----------')
-print('Total number Asian ', total_number_of_race_Asian())
-print('\n')
-
-print('--------Total Number Of American Indian or Alaska Native-----------')
-print('Total number American Indian or Alaska Native ', total_number_of_race_AmericanIndian())
-print('\n')
-
-print('--------Total Number Of Native Hawaiian or Other Pacific Islander-----------')
-print('Total number Native Hawaiian or Other Pacific Islander ', total_number_of_race_NativeHawiian())
-print('\n')
-
-print('--------Total Number Of Multiple Race-----------')
-print('Total number Multiple Race ', total_number_of_race_Multiple())
-print('\n')
-
-print('--------Total Number Of Race Known-----------')
-print('Total number of Race Known ', total_number_of_race_known())
-print('\n')
-
-print("---------Chronically Homeless (Veteran Only)---------")
-print('\n')
-
-
-# ##* Ask About the correct value 
-print('--------Total number of households (Chronically Homeless)-----------')
-print('Total number of households (Chronically Homeless): ', total_number_of_ChronicallyHomeless())
-print('\n')
-
-print('--------Total number of persons(Chronically Homeless)-----------')
-print('Total number of persons (Chronically Homeless): ', total_number_person_chronically_homeless())
-print('\n')
-
-
+root= tk.Tk()
+root.title('Menu')
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'seashell3', relief = 'raised')
+canvas1.pack()
+saveAsButton_CSV = tk.Button(text='Export CSV', command=exportCSV, bg='red', fg='black', font=('helvetica', 15, 'bold'))
+loadAsButton_CSV = tk.Button(text='Import CSV', command=importCSV, bg='red', fg='black', font=('helvetica', 15, 'bold'))
+cancelButton = tk.Button(text='Cancel',command=root.destroy ,bg='red', fg='black', font=('helvetica', 15, 'bold'))
+canvas1.create_window(75, 150, window=loadAsButton_CSV)
+canvas1.create_window(225, 150, window=saveAsButton_CSV)
+canvas1.create_window(150, 225, window=cancelButton)
+root.mainloop()
 
