cf685936026117ab652df5609760d870a8e03348
Josiah Lee <josiahwlee@gmail.com>
2019-08-11 13:15:43 -0700

    Changed citytables to pull from backend rather than parsing csvs

--- RCHIWebDash/RCHIWebDashBoard/src/components/CityTables/CityTables.js
+++ RCHIWebDash/RCHIWebDashBoard/src/components/CityTables/CityTables.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './CityTables.css'
-import Papa from 'papaparse'
 import ReactToPrint from 'react-to-print';
 import Select from 'react-select'
+import axios from 'axios';
+import { citynames, base } from './constants';
 
 export default class CityTables extends Component {
     constructor(props){
@@ -10,68 +11,18 @@ export default class CityTables extends Component {
         this.state = {
             data: {},
             curCities: ['RIVERSIDE'],
-            dataNum: 0,
             households: {},
-            citynames: [
-                'BANNING',				
-                'BEAUMONT',
-                'BLYTHE',
-                'CALIMESA',
-                'CATHEDRAL CITY',
-                'COACHELLA',
-                'CORONA',
-                'DESERT HOT SPRINGS',
-                'HEMET',
-                'INDIAN WELLS',
-                'INDIO',
-                'JURUPA VALLEY',
-                'LA QUINTA',
-                'LAKE ELSINORE',
-                'MENIFEE',
-                'MORENO VALLEY',
-                'MURRIETA',
-                'NORCO',
-                'PALM DESERT',
-                'PALM SPRINGS',
-                'PERRIS',
-                'RANCHO MIRAGE',
-                'RIVERSIDE',
-                'RIVERSIDE 2',
-                'RIVERSIDE 1',
-                'SAN JACINTO',
-                'TEMECULA',
-                'WILDOMAR',
-                'UNINCORPORATED 1',
-                'UNINCORPORATED 2',
-                'UNINCORPORATED 3',
-                'UNINCORPORATED 4',
-                'UNINCORPORATED 5',
-            ],
+            citynames: citynames,
             selectOptions: []
           };
-        this.updateData = this.updateData.bind(this);
-        this.updateHouseholdData = this.updateHouseholdData.bind(this);
-        this.createTable = this.createTable.bind(this);
     }
+
     componentWillMount() {
 		/* 
-			When its determined that the component will mount this parses all the csvs needed for the Supervisorial Districts and places them into state
+			calls fetch data functions
 		*/
-        var csvFilePath = require("./2019citySubPopulations.csv");
-        Papa.parse(csvFilePath, {
-            header: true,
-            download: true,
-            skipEmptyLines: true,
-            complete: this.updateData
-        });
-
-        csvFilePath = require("./2019_cityHouseholds_modified.csv");
-        Papa.parse(csvFilePath, {
-            header: true,
-            download: true,
-            skipEmptyLines: true,
-            complete: this.updateHouseholdData
-        });
+        this.fetchSubpopulationData();
+        this.fetchHouseholdData();
 
         var options = [];
         for (var i = 0; i < this.state.citynames.length; i++)
@@ -87,26 +38,29 @@ export default class CityTables extends Component {
         })
     }
 
-    updateHouseholdData(csvData){
-        const indata = csvData.data;
-        var data = {};
-        //console.log(indata);
-        for (var i = 0; i < this.state.citynames.length ; i++)
+    async fetchHouseholdData() {
+        const promise = await axios.get("http://localhost:8000/api/HouseholdsByCity2019/");
+        const indata = promise.status===200 ? promise.data: [];
+        if (indata.length === 0){
+            throw Error;
+        }
+        let data = {};
+        const {citynames} = this.state;
+        for (let i = 0; i < citynames.length ; i++)
         {
-            data[this.state.citynames[i]] = { 
+            data[citynames[i]] = { 
                 'Total Households' :0,
                 'Adults Only':0,
                 'Children Only':0,	
                 'Adults and Children':0
             }
         }
-        for (var i = 0; i < indata.length ; i++)
+        for (let i = 0; i < indata.length ; i++)
         {
-            //console.log(indata[i])
-            data[indata[i]['City']]['Total Households'] = indata[i]['Total Households'];
-            data[indata[i]['City']]['Adults Only'] = indata[i]['Adults Only'];
-            data[indata[i]['City']]['Children Only'] = indata[i]['Children Only'];
-            data[indata[i]['City']]['Adults and Children'] = indata[i]['Adults and Children'];
+            data[indata[i]['city']]['Total Households'] = indata[i]['totalHouseholds'];
+            data[indata[i]['city']]['Adults Only'] = indata[i]['adultsOnly'];
+            data[indata[i]['city']]['Children Only'] = indata[i]['childrenOnly'];
+            data[indata[i]['city']]['Adults and Children'] = indata[i]['adultsAndChildren'];
             
         }
         this.setState({
@@ -114,102 +68,30 @@ export default class CityTables extends Component {
         })
     }
 
-    updateData(csvData) {
-        const indata = csvData.data;
+    async fetchSubpopulationData() {
+        const promise = await axios.get("http://localhost:8000/api/SubpopulationsByCity2019/");
+        const indata = promise.status===200 ? promise.data: [];
+        if (indata.length === 0){
+            throw Error;
+        }
         var data = {};
         for (var i = 0; i < this.state.citynames.length ; i++)
         {
-            data[this.state.citynames[i]] = {
-                'District': 0,
-                'Age':	{
-                    'Youth (18-24)': { 'Interview':0, 'Observation':0 },
-                    'Children (<18)': { 'Interview':0, 'Observation':0 },
-                    'Adults (>24)': { 'Interview':0, 'Observation':0 },
-                    //'Elderly (62+)': { 'Interview':0, 'Observation':0 },
-                    'Unknown Age': { 'Interview':0, 'Observation':0 },
-                    'Total': { 'Interview':0, 'Observation':0 },
-                },
-                'Race':	{
-                    'Asian': { 'Interview':0, 'Observation':0 },
-                    'Black': { 'Interview':0, 'Observation':0 },
-                    'White': { 'Interview':0, 'Observation':0 },
-                    'American Indian': { 'Interview':0, 'Observation':0 },
-                    'Native Hawaiian': { 'Interview':0, 'Observation':0 },
-                    'Multiple Races': { 'Interview':0, 'Observation':0 },
-                    'Unknown Race': { 'Interview':0, 'Observation':0 },
-                    'Total': { 'Interview':0, 'Observation':0 },
-                },
-                'Ethinicity':	{
-                    'Hispanic': { 'Interview':0, 'Observation':0 },
-                    'NonHispanic': { 'Interview':0, 'Observation':0 },
-                    'Unknown Ethinicity': { 'Interview':0, 'Observation':0 },
-                    'Total': { 'Interview':0, 'Observation':0 },
-                },
-                'Gender':	{
-                    'Male': { 'Interview':0, 'Observation':0 },
-                    'Female': { 'Interview':0, 'Observation':0 },
-                    'Transgender': { 'Interview':0, 'Observation':0 },
-                    'Gender Non-Conforming': { 'Interview':0, 'Observation':0 },
-                    'Unknown Gender': { 'Interview':0, 'Observation':0 },
-                    'Total': { 'Interview':0, 'Observation':0 },
-                },
-                'Subpopulations':	{
-                    'Veteran Yes': { 'Interview':0, 'Observation':0 },
-                    'Veteran No': { 'Interview':0, 'Observation':0 },
-                    'Chronically Homeless': { 'Interview':0, 'Observation':0 },
-                    'Not Chronically Homeless': { 'Interview':0, 'Observation':0 },
-                    'Substance Abuse': { 'Interview':0, 'Observation':0 },
-                    'No Substance Abuse': { 'Interview':0, 'Observation':0 },
-                    'Unknown Substance Abuse': { 'Interview':0, 'Observation':0 },
-                    'PTSD': { 'Interview':0, 'Observation':0 },
-                    'No PTSD': { 'Interview':0, 'Observation':0 },
-                    'Unknown PTSD': { 'Interview':0, 'Observation':0 },
-                    'Mental Health Conditions': { 'Interview':0, 'Observation':0 },
-                    'No Mental Health Conditions': { 'Interview':0, 'Observation':0 },
-                    'Unknown Mental Health Conditions': { 'Interview':0, 'Observation':0 },
-                    'Physical Disability': { 'Interview':0, 'Observation':0 },
-                    'No Physical Disability': { 'Interview':0, 'Observation':0 },
-                    'Unknown Physical Disability': { 'Interview':0, 'Observation':0 },
-                    'Developmental Disability': { 'Interview':0, 'Observation':0 },
-                    'No Developmental Disability': { 'Interview':0, 'Observation':0 },
-                    'Unknown Developmental Disability': { 'Interview':0, 'Observation':0 },
-                    'Brain Injury': { 'Interview':0, 'Observation':0 },
-                    'No Brain Injury': { 'Interview':0, 'Observation':0 },
-                    'Unknown Brain Injury': { 'Interview':0, 'Observation':0 },
-                    'Victim of Domestic Violence': { 'Interview':0, 'Observation':0 },
-                    'Not Victim of Domestic Violence': { 'Interview':0, 'Observation':0 },
-                    'Unknown Victim of Domestic Violence': { 'Interview':0, 'Observation':0 },
-                    'AIDS or HIV': { 'Interview':0, 'Observation':0 },
-                    'No AIDS or HIV': { 'Interview':0, 'Observation':0 },
-                    'Unknown AIDS or HIV': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 90 Days: Probation': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 90 Days: Parole': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 90 Days: Completed Sentence': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 90 Days: (Unspecified)': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 12 Months: Probation': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 12 Months: Parole': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 12 Months: Completed Sentence': { 'Interview':0, 'Observation':0 },
-                    'Jail Release 12 Months: (Unspecified)': { 'Interview':0, 'Observation':0 },
-                    'No Jail': { 'Interview':0, 'Observation':0 },
-                    'Unknown Jail': { 'Interview':0, 'Observation':0 },
-                    //'Families with Children': { 'Interview':0, 'Observation':0 },
-                    'Total': { 'Interview':0, 'Observation':0 },
-                },	
-        }
+            data[this.state.citynames[i]] = JSON.parse(JSON.stringify(base));
         }
 
         for (var i = 0; i < indata.length ; i++)
         {
-            data[indata[i]['CityName']]['District'] = indata[i]['District'];
-            data[indata[i]['CityName']][indata[i]['Catergory']][indata[i]['Subpopulation']]['Interview'] = indata[i]['Interview'];
-            data[indata[i]['CityName']][indata[i]['Catergory']][indata[i]['Subpopulation']]['Observation'] = indata[i]['Observation'];
+            data[indata[i]['city']]['District'] = indata[i]['district'];
+            data[indata[i]['city']][indata[i]['category']][indata[i]['subpopulation']]['Interview'] = indata[i]['interview'];
+            data[indata[i]['city']][indata[i]['category']][indata[i]['subpopulation']]['Observation'] = indata[i]['observation'];
         }
-
         this.setState({
             data: data
         })
     }
-    createTable(incity){
+
+    createTable = (incity) => {
         var Catergories = [
             'Race',
             'Ethinicity',
@@ -346,7 +228,7 @@ export default class CityTables extends Component {
             var a = 0, curTot = 0, int = 0, obs = 0;
             if (Object.keys(this.state.data).length > 0)
             {
-            // creates table rowa then pushes to table
+            // creates table row then pushes to table
                 for (var i = 0; i < 4; i++)
                 {
                     tableRow = []
@@ -461,8 +343,8 @@ export default class CityTables extends Component {
         return table;
     }
     render(){
-        //console.log(this.state.households);
-        //console.log(this.state.data);
+        // console.log(this.state.households);
+        // console.log(this.state.data);
         return(
             <div className="row">
                 <div className="col-sm-3 leftCol">
@@ -475,7 +357,7 @@ export default class CityTables extends Component {
                     />
                 </div>
                 <div className="col-sm-9 tableCol">
-                    <table ref={el => (this.componentRef = el)} className="paperSize">
+                    <table ref={el => (this.componentRef = el)} className="paperSize" key={Math.random()}>
                         {/* <caption>*percents for subpopulations are taken from totals within each catergory (i.e. Veterans/ Total Veterans in City)</caption> */}
                         <thead>
                             <tr>
@@ -491,7 +373,7 @@ export default class CityTables extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {this.createTable(this.state.curCity)}
+                            {this.createTable(this.state.curCities[0])}
                         </tbody>
                     </table>
                 </div>
