d59134a5cf93d01848a4d9f72bccbd8aa4ec99bd
Francisco Gallego <fgall002@ucr.edu>
2019-10-10 14:07:25 -0700

    Modified the way the data is formated. In order for it to work for Nivo. Might Still make some ticks later

--- RCHIWebDash/RCHIWebDashBoard/src/components/UnshelteredTrendsPage/Trends/YouthTrend.js
+++ RCHIWebDash/RCHIWebDashBoard/src/components/UnshelteredTrendsPage/Trends/YouthTrend.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import LineChart from '../../Utilities/GraphTypes/Line'
+import NivoLineChart from '../../Utilities/GraphTypes/NivoLine'
 
 class YouthGraph extends Component {
-    constructor(){
+    constructor(props){
         super();
         this.state = {
             chartData:null,
@@ -11,64 +11,55 @@ class YouthGraph extends Component {
     }
 
     componentDidMount(){
-        axios.get('http://localhost:8000/api/SubpopulationsByYear/?search=Youth%20(18-24)') 
+        axios.get('http://localhost:8000/api/SubpopulationsByYear/?search='+ this.props.query) 
             .then(response => {
 
-                const formatedData = response.data.filter(row => row.sheltered === false).map(row => {
-                    return (
-                        {
-                            title: row.year,
-                            categories: { 
-                                Total: row.interview + row.observation,
-                                Interviewed: row.interview,
-                                Observed: row.observation
+                var completeData = []
+                const filterData = response.data.filter(index => index.sheltered === false)
+
+                const categories = ['Total', 'Interviewed', 'Observed']
+
+                for(let i = 0; i< categories.length; i++){
+                    const data = filterData.map(index => {
+                        if(categories[i] === 'Total'){
+                            return {
+                                "x" : index.year,
+                                "y" : index.observation + index.interview
                             }
                         }
-                    )
-                })
+                        else if(categories[i] === 'Interviewed'){
+                            return {
+                                "x" : index.year,
+                                "y" : index.interview
+                            }
+                        }
+                        else if(categories[i] === 'Observed'){
+                            return {
+                                "x" : index.year,
+                                "y" : index.observation
+                            }
+                        }
 
+                    })
+
+                    completeData.push({"id": categories[i],"data": data})
+                }
+
+                console.log("Complete Data: ", completeData)
 
                 this.setState({
-                    chartData: {
-                        labels: formatedData.map( index => index.title),
-                        datasets:[
-                            {
-                                label:'Total',
-                                data: formatedData.map(index => index.categories.Total),
-                                backgroundColor:'rgba(75,192,192,0.6)',
-                                borderWidth:5,
-                                fill:false,
-                                borderColor:'rgba(75,192,192,0.6)'
-                            },
-                            {
-                                label:'Interviewed',
-                                data: formatedData.map(index => index.categories.Interviewed),
-                                backgroundColor:'rgba(153,102,255,0.6)',
-                                borderWidth:5,
-                                fill:false,
-                                borderColor:'rgba(153,102,255,0.6)'
-                            },
-                            {
-                                label:'Observed',
-                                data: formatedData.map(index => index.categories.Observed),
-                                backgroundColor:'rgba(255,159,64,0.6)',
-                                borderWidth:5,
-                                fill:false,
-                                borderColor:'rgba(255,159,64,0.6)'
-                            }
-                        ]
-                    }
+                    chartData: completeData
                 })
             })
     }
 
     render(){
         if(!this.state.chartData){
-            return <h1>Waiting for data</h1>
+            return <h1></h1>
         }
         if(this.state.chartData){
             return(
-                <LineChart charData={this.state.chartData} displayLegend={true} title="Youth (18-24)" legendPosition="bottom"/>
+                    <NivoLineChart  subHeader={this.props.subHeader} header={this.props.header} data={this.state.chartData}/>
             )
         }
     }
