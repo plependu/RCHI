0fc8d07cbae18c85f269f6d2f02d7cfe214c3da4
Francisco Gallego <fgall002@ucr.edu>
2019-10-10 14:04:09 -0700

    Fixed backend error regardin module for backend.url is not found

--- RCHIWebDash/RCHIWebDash/urls.py
+++ RCHIWebDash/RCHIWebDash/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('backend.urls')),
+     path('api/', include('backend.api.urls'))
 ]
