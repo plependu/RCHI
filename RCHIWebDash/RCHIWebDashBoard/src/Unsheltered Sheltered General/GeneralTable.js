841fc515ecf53de96b269b8b3adac73c0c49c1bc
Kevin F <krferrer11@gmail.com>
2019-12-05 23:26:09 -0800

    created table component

--- RCHIWebDash/RCHIWebDashBoard/src/Unsheltered Sheltered General/GeneralTable.js
+++ RCHIWebDash/RCHIWebDashBoard/src/Unsheltered Sheltered General/GeneralTable.js
@@ -3,28 +3,66 @@ import React, {Component} from 'react'
 import BarGraph from '../components/TestingBranch/BarGraph'
 import PieGraph from "../components/charts/PieGraph";
 import PieChart2 from "../components/charts/PieChart2"
+import TableComponent from "../components/charts/TableComponent"
 
 export default class GeneralTable extends Component{
     constructor(props){
         super(props)
 
-
+        this.const_data_for_unsheltered_table = [
+          {
+            "id":1,
+            "subpopulation":"Individuals",
+            "year":"2018",
+            "total":1685
+          },
+          {
+            "id":2,
+            "subpopulation":"Adults(>=25)",
+            "year":"2018",
+            "total":1350
+          },
+          {
+            "id":3,
+            "subpopulation":"Individuals",
+            "year":"2019",
+            "total":2045
+          },
+          {
+            "id":4,
+            "subpopulation":"Adults(>=25)",
+            "year":"2019",
+            "total":1718
+          },
+          {
+            "id":5,
+            "subpopulation":"Youth(18-24)",
+            "year":"2018",
+            "total":177
+          },
+          {
+            "id":6,
+            "subpopulation":"Youth(18-24)",
+            "year":"2019",
+            "total":181
+          }
+        ];
     }
 
     componentDidMount(){
-        
+
     }
     render(){
         console.log("rendering")
         return(
         <div>
-            <BarGraph height = {600} 
-            width = {800} 
+            <BarGraph height = {600}
+            width = {800}
             url = 'http://127.0.0.1:8000/api/GeneralTableSubpopulations2019/?search=Race'
             indexBy = "subpopulation"
             keys = {["interview", "observation"]}
-            
-            /> 
+
+            />
 
             <PieGraph height = {500}
             url = 'http://127.0.0.1:8000/api/GeneralTableSubpopulations2019/?search=Gender'
@@ -34,11 +72,17 @@ export default class GeneralTable extends Component{
             url = 'http://127.0.0.1:8000/api/GeneralTableSubpopulations2019/?search=households'
             />
 
-            
+
             <PieChart2 height = {500}
             url = "http://127.0.0.1:8000/api/GeneralTableSubpopulationsSheltered2019/?search=Ethnicity"
             />
 
+            <TableComponent
+              data = {this.const_data_for_unsheltered_table}
+              year1 = {2018}
+              year2 = {2019}
+              tableName = "Unsheltered Statistics"
+            />
 
 
         </div>
---
+++ RCHIWebDash/RCHIWebDashBoard/src/components/charts/TableComponent.js
@@ -0,0 +1,148 @@
+import React, {Component} from 'react';
+import { Header, Table} from 'semantic-ui-react';
+
+class TableComponent extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      chartData : null,
+      year1 : null,
+      year2 : null,
+      filteredData: [],
+      body: null,
+      flag: false
+    }
+  }
+
+
+
+
+
+  componentDidMount(){
+    //Arrange Data
+
+    this.state.chartData = this.props.data;
+    this.state.year1 = this.props.year1;
+    this.state.year2 = this.props.year2;
+
+
+    // for(let i  = 0; i < this.state.chartData.length; ++i){
+    //   console.log(this.state.chartData[i]);
+    // }
+
+    var label_array = [];
+    var unpopulated_array = [];
+
+    //fill label_array
+    for(let i = 0; i < this.state.chartData.length; ++i){
+      if(!label_array.includes(this.state.chartData[i].subpopulation)){
+        label_array.push(this.state.chartData[i].subpopulation);
+      }
+    }
+
+    //fill unpopulated_array
+    for(let i = 0; i < label_array.length; ++i){
+      var temp = {
+        "rowName": label_array[i],
+        "value1": -1,
+        "value2": -2
+      }
+      unpopulated_array.push(temp);
+    }
+
+    // for(let i = 0; i < unpopulated_array.length; ++i){
+    //   console.log(unpopulated_array[i]);
+    // }
+
+    //If year1 is lower
+    if(this.state.year1 < this.state.year2){
+      //populate unpopulated_arrays
+      for(let i = 0; i < this.state.chartData.length; ++i){
+        //check if in list1
+        if(label_array.includes(this.state.chartData[i].subpopulation)){
+          //find it in list2
+
+          for(let j = 0; j < unpopulated_array.length; ++j){
+            if(this.state.chartData[i].subpopulation == unpopulated_array[j].rowName){
+              //check year
+              if(this.state.chartData[i].year == this.state.year1){
+                unpopulated_array[j].value1 = this.state.chartData[i].total;
+              }
+              else if(this.state.chartData[i].year == this.state.year2){
+                unpopulated_array[j].value2 = this.state.chartData[i].total;
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+    for(let i = 0; i < unpopulated_array.length; ++i){
+      console.log(unpopulated_array[i]);
+    }
+
+    this.state.filteredData = unpopulated_array;
+
+
+    this.setState(
+      {
+        year1 : this.props.year1,
+        year2 : this.props.year2,
+        filteredData: unpopulated_array,
+        flag : true
+      }
+    );
+    console.log(this.state.filteredData);
+  }
+
+  render(){
+    if(!this.state.flag){
+      return(<h3>Loading</h3>);
+    }
+    else{
+    return(
+      <div>
+        <Table Cell Structured>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan='3' textAlign='center'>
+                <Header>
+                  {this.props.tableName}
+                  <Header.Subheader/>
+                </Header>
+              </Table.HeaderCell>
+            </Table.Row>
+            <Table.Row>
+              <Table.HeaderCell textAlign='center'></Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>{this.state.year1}</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>{this.state.year2}</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {
+              this.state.filteredData.map( (iterator, idx)=>{
+                  return(
+                    <Table.Row>
+                      <Table.HeaderCell textAlign='center'>{this.state.filteredData[idx].rowName}</Table.HeaderCell>
+                      <Table.HeaderCell textAlign='center'>{this.state.filteredData[idx].value1}</Table.HeaderCell>
+                      <Table.HeaderCell textAlign='center'>{this.state.filteredData[idx].value2}</Table.HeaderCell>
+                    </Table.Row>
+                  );
+              })
+            }
+          </Table.Body>
+
+        </Table>
+        {console.log(this.state.filteredData)}
+        {console.log(this.state.filteredData[0].rowName)}
+      </div>
+    )
+  }
+  }
+}
+
+
+export default TableComponent
