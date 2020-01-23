8b2eae93685213dd2cdd8c55310437e9ee194f39
jwlee64 <josiahwlee@gmail.com>
2020-01-23 15:25:37 -0800

    added a get function, changed filter to remove for clarity

--- RCHIWebDash/RCHIWebDashBoard/src/components/Utilities/ListManipulation/filter.js
+++ RCHIWebDash/RCHIWebDashBoard/src/components/Utilities/ListManipulation/filter.js
@@ -1,9 +1,20 @@
-// filters a single dataArray
-export function filter ( data, catergory, value ) {
+// from a list get all values whose category equals the value
+export function get ( data, category, value ) {
     var newData = [];
-    for ( const x in data ) {
-        if ( x[catergory] != value ) {
-            newData.push(x);
+    for ( var x = 0; x < data.length; x++ ) {
+        if ( data[x][category] == value ) {
+            newData.push(data[x]);
+        }
+    }
+    return newData;
+}
+
+// remove all entried when the category equals value from a list
+export function remove ( data, category, value ) {
+    var newData = [];
+    for ( var x = 0; x < data.length; x++ ) {
+        if ( data[x][category] != value ) {
+            newData.push(data[x]);
         }
     }
     return newData;
