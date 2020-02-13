3cad34524b497198effc63f9d117022bd196f0dc
Josiah Lee <josiahwlee@gmail.com>
2020-02-12 21:29:23 -0800

    modified colors

--- RCHIWebDash/RCHIWebDashBoard/src/components/Utilities/colors.js
+++ RCHIWebDash/RCHIWebDashBoard/src/components/Utilities/colors.js
@@ -8,45 +8,48 @@ export const colors = [
     [
         '#BF99B2', // Light Purple
         '#B2C7F6', // Light Blue
-        '#807F85', // Grey
-    ],
-    [
-        '#BF99B2', // Light Purple
-        '#B2C7F6', // Light Blue
-        '#807F85', // Grey
-        '#BA8C9F', // Medium Purple
-    ],
-    [
-        '#BF99B2', // Light Purple
-        '#B2C7F6', // Light Blue
-        '#807F85', // Grey
-        '#BA8C9F', // Medium Purple
-        '#533747', // Dark Purple
-    ],
-    [
-        '#BF99B2', // Light Purple
-        '#B2C7F6', // Light Blue
-        '#807F85', // Grey
-        '#BA8C9F', // Medium Purple
-        '#533747', // Dark Purple
         '#6D7CA6', // Purple Blue
+        '#807F85', // Grey
     ],
     [
         '#BF99B2', // Light Purple
         '#B2C7F6', // Light Blue
+        '#6D7CA6', // Purple Blue
+        '#807F85', // Grey
+        '#BA8C9F', // Medium Purple
+    ],
+    [
+        '#BF99B2', // Light Purple
+        '#B2C7F6', // Light Blue
+        '#6D7CA6', // Purple Blue
         '#807F85', // Grey
         '#BA8C9F', // Medium Purple
         '#533747', // Dark Purple
+    ],
+    [
+        '#BF99B2', // Light Purple
+        '#B2C7F6', // Light Blue
         '#6D7CA6', // Purple Blue
+        '#807F85', // Grey
+        '#BA8C9F', // Medium Purple
+        '#533747', // Dark Purple
+    ],
+    [
+        '#BF99B2', // Light Purple
+        '#B2C7F6', // Light Blue
+        '#6D7CA6', // Purple Blue
+        '#807F85', // Grey
+        '#BA8C9F', // Medium Purple
+        '#533747', // Dark Purple
         '#2F3C4D', // Dark Blue
     ],
     [
         '#BF99B2', // Light Purple
         '#B2C7F6', // Light Blue
+        '#6D7CA6', // Purple Blue
         '#807F85', // Grey
         '#BA8C9F', // Medium Purple
         '#533747', // Dark Purple
-        '#6D7CA6', // Purple Blue
         '#2F3C4D', // Dark Blue
         '#E15759', // Red
     ]
